35ca3003e2f396ccbe2d543bd48c47d0b37f8a2a
leandrohirschmann <lleandrosh@gmail.com>
2014-02-24 09:43:03 -0300

    Atualização HelloYou

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World !!!!!");
+		Scanner ler = new Scanner(System.in);
+		String you = ""; 
+		System.out.println("Quem é você ???");
+		you = ler.next();
+		System.out.print("Hello "+you+"!!!!");
 
 	}
 
